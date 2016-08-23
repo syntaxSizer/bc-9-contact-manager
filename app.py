ec7412987d595857711a3925d46938de05aff0b8
syntaxSizer <kakuma4never@gmail.com>
2016-08-23 22:18:31 +0300

    modules:  add new contact and search  modules

--- app.py
+++ app.py
@@ -2,22 +2,20 @@
 # interactive command application.
 """
 Usage:
-    contact_manager -n <name> -p <phonenumber> [--timeout=<seconds>]
+    contact_manager -n <name>  -p <phonenumber> [--timeout=<seconds>]
     contact_manager search <name> [--timeout=<seconds>]
     contact_manager text <name> -m <message> [--timeout=<seconds>]
     contact_manager serial <port> [--baud=<n>] [--timeout=<seconds>]
     contact_manager (-i | --interactive)
     contact_manager (-h | --help | --version)
 Options:
-    -i, --interactive  Interactive Mode
-    -h, --help  Show this screen and exit.
-    --baud=<n>  Baudrate [default: 9600]
+    -i, --interactive        Interactive Mode
+    -h, --help               Show this screen and exit.
 """
-import sys, cmd
+
+import sys
+import cmd
 from docopt import docopt, DocoptExit
-
-
-
 
 
 def docopt_cmd(func):
@@ -25,6 +23,7 @@ def docopt_cmd(func):
     This decorator is used to simplify the try/except block and pass the result
     of the docopt parsing to the called action.
     """
+
     def fn(self, arg):
         try:
             opt = docopt(fn.__doc__, arg)
@@ -50,21 +49,51 @@ def docopt_cmd(func):
     fn.__dict__.update(func.__dict__)
     return fn
 
+
 class MyInteractive (cmd.Cmd):
     intro = 'Welcome to my interactive program!' \
         + ' (type help for a list of commands.)'
     prompt = '(contact_manager) '
     file = None
 
-       def add_contact(self, name, number):
-        new_contact = ContactEntry(name,number)
+    def add_contact(self, name, number):
+        new_contact = ContactEntries(name, number)
         new_contact.add_contact()
 
     def search(self, name):
         search_item = ContactSearch(name)
         search_item.search_contact_list()
-        
 
     def sms(self, name, message):
         send_msg = SendSms(name, message)
         send_msg.send_sms()
+
+    @docopt_cmd
+    def do_add(self, args):
+        """Usage: add -n <name> -p <phonenumber>"""
+        # print/ args['<name>'], "number is ", args['<phonenumber>']
+        self.add_contact(args['<name>'], args['<phonenumber>'])
+
+    @docopt_cmd
+    def do_search(self, args):
+        """Usage: search search <name>"""
+        self.search(args['<name>'])
+
+    @docopt_cmd
+    def do_text(self, args):
+        """Usage: text <name> -m <message>..."""
+
+        self.sms(args['<name>'], (" ".join(args['<message>'])))
+
+    def do_quit(self, args):
+        """Quits out of Interactive Mode."""
+
+        print('Awesome saving contacts')
+        exit()
+
+opt = docopt(__doc__, sys.argv[1:])
+
+if opt['--interactive']:
+    MyInteractive().cmdloop()
+
+print(opt)
---
+++ contacts.py
@@ -0,0 +1,14 @@
+from database import Database
+
+
+class ContactEntries:
+	"""docstring for Contacts"""
+
+	def __init__(self, name, my_number):
+		self.name= name
+		self.my_number = my_number
+
+		def add_contact(self):
+			contact_list = {}
+			
+	

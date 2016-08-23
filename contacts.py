87709537fc23ccf2843969b950f30ca02b78b868
syntaxSizer <kakuma4never@gmail.com>
2016-08-23 22:25:19 +0300

    fix indentation error

--- contacts.py
+++ contacts.py
@@ -1,22 +1,20 @@
 from database import Database
 
-
 class ContactEntries:
-    """save contacts in the db"""
+        """save contacts in the db"""
 
-    def __init__(self, name, my_number):
-        self.name = name
-        self.my_number = my_number
+        def __init__(self, name, my_number):
+            self.name = name
+            self.my_number = my_number
 
         def add_contact(self):
-        	"""
-        	save the contact in a dict with the phone number as a key
-        	"""
+            """
+            save the contact in a dict with the phone number as a key
+            """
             contact_dict = {}
             contact_dect[self.my_number] = self.name
             db_connect = Database()
             db_connect.add_contact(self.name, self.my_number)
-
 
 class ContactSearch:
 
@@ -34,3 +32,4 @@ class ContactSearch:
             # print str(i[1]), i[2]
             print values
         return result
+    

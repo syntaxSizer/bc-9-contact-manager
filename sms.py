ab552ba6738f82b73fb06a070c96490241e9f517
syntaxSizer <kakuma4never@gmail.com>
2016-08-23 23:27:42 +0300

    API: add username and api key

---
+++ sms.py
@@ -0,0 +1,10 @@
+from AfricasTalkingGateway import AfricasTalkingGateway
+from contacts import ContactSearch
+
+
+class Sendsms:
+
+	USERNAME = 'syntaxsizer' 
+	APIKEY	 = '34ca99d2a31142942ae5807e93aa795aa3489240b2d3f6bd3b203719c754ea5d'
+
+	def __init__(self):
